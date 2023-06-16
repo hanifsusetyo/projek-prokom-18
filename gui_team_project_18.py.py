a7db66d875154c2f96b818140b10ebc6d2f37609
hanifsusetyo <127179190+hanifsusetyo@users.noreply.github.com>
2023-06-16 12:17:27 +0700

    Update gui_team_project_18.py

--- gui_team_project_18.py.py
+++ gui_team_project_18.py.py
@@ -3,8 +3,9 @@ from tabulate import tabulate
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
+from PIL import Image, ImageTk
 
-file = 'database_makanan_coba.json'
+file = 'database_makanan_revisi.json'
 user_file = 'registered_users.json'
 
 def register():
@@ -13,6 +14,14 @@ def register():
 
     if username != "" and password != "":
         user_data = {'username': username, 'password': password}
+
+        with open(user_file, 'r') as file_obj:
+            for line in file_obj:
+                data = json.loads(line)
+                if data['username'] == username:
+                    messagebox.showinfo("Info", "Anda sudah terdaftar! Silakan login.")
+                    return
+
         with open(user_file, 'a') as file_obj:
             json.dump(user_data, file_obj)
             file_obj.write('\n')
@@ -20,7 +29,6 @@ def register():
         messagebox.showinfo("Info", "Registrasi berhasil! Silakan login dengan akun yang telah didaftarkan.")
     else:
         messagebox.showerror("Error", "Username dan password tidak boleh kosong.")
-
 def login():
     username = entry_username.get()
     password = entry_password.get()
@@ -39,6 +47,8 @@ def get_masakan_by_harga():
     global file
     global hasil_pencarian
     budget = int(entry_budget.get())
+    if budget > 50000:
+        messagebox.showinfo("Info", "Batas Budget Adalah 50.000")
     kategori = entry_kategori.get().lower()
 
     with open(file, 'r') as file_obj:
@@ -75,30 +85,42 @@ def print_masakan(masakan):
     else:
         return None
 
-def print_resep():
-    masakan = hasil_pencarian 
-    
+def print_resep(hasil_pencarian):
+    masakan = hasil_pencarian
+
     if masakan:
         pilihan = int(entry_pilihan.get())
         if 1 <= pilihan <= len(masakan):
             masakan_pilihan = masakan[pilihan - 1]
             resep = masakan_pilihan['Resep']
+            
+            resep_tab = tk.Toplevel(root)
+            resep_tab.title("Resep " + masakan_pilihan['Nama'])
+            resep_tab.geometry("1280x720")
 
-            bahan = resep['Bahan']
-            instruksi = resep['Instruksi']
+            background_image_resep = Image.open("makanan.jpg")
+            background_image_resep = background_image_resep.resize((1280, 720), Image.ANTIALIAS)
+            background_photo_resep = ImageTk.PhotoImage(background_image_resep)
 
-            result_text.config(state='normal')
-            result_text.insert(tk.END, f"\n\nResep {masakan_pilihan['Nama']}:\n")
-            result_text.insert(tk.END, "Bahan:\n")
-            result_text.insert(tk.END, bahan)
-            result_text.insert(tk.END, "\n\nInstruksi:\n")
-            result_text.insert(tk.END, instruksi)
-            result_text.config(state='disabled')
+            resep_text = tk.Text(resep_tab, height=60, width=80)
+            resep_text.pack()
+
+            resep_text.insert(tk.END, f"\n\nResep {masakan_pilihan['Nama']}:\n")
+            resep_text.insert(tk.END, "Bahan:\n")
+            
+            bahan_table = [[i + 1, bahan, harga] for i, (bahan, harga) in enumerate(zip(resep['Bahan'], masakan_pilihan['Harga']))]
+            bahan_headers = ["No.", "Bahan", "Harga"]
+            bahan_table_data = tabulate(bahan_table, headers=bahan_headers)
+            
+            resep_text.insert(tk.END, bahan_table_data)
+            resep_text.insert(tk.END, "\n\nInstruksi:\n")
+            resep_text.insert(tk.END, resep['Instruksi'])
+            resep_text.config(state='disabled')
         else:
-            messagebox.showerror("Error", "Masakan tidak valid.")
+            messagebox.showerror("Error", "Masukkan Angka Yang Sesuai.")
     else:
         messagebox.showinfo("Info", "Tidak ada masakan yang sesuai dengan kriteria.")
-        
+
 def search_again():
     entry_budget.delete(0, tk.END)
     entry_kategori.delete(0, tk.END)
@@ -107,69 +129,101 @@ def search_again():
     result_text.delete("1.0", tk.END)
     result_text.config(state='disabled')
 
+def log_out():
+    login_window.deiconify()
+    root.withdraw()
+    entry_username.delete(0, tk.END)
+    entry_password.delete(0, tk.END)
+    entry_budget.delete(0, tk.END)
+    entry_kategori.delete(0, tk.END)
+    entry_pilihan.delete(0, tk.END)
+    result_text.config(state='normal')
+    result_text.delete("1.0", tk.END)
+    result_text.config(state='disabled')
+
 root = tk.Tk()
-root.title("Program Resep Masakan")
-root.geometry("600x500")
+root.title("Program Masak on Budget")
+root.geometry("1280x720")
 
 style = ttk.Style()
 style.theme_use("clam")
+style.configure("Biru.TButton", background="blue", foreground="white")
+style.configure("Merah.TButton", background="red", foreground="white")
+style.configure("Custom.TLabel", background="white", foreground="black")
+style.configure("Green.TButton", background="green", foreground="white")
+
+background_image = Image.open("makanan.jpg")
+background_image = background_image.resize((1280, 720), Image.ANTIALIAS)
+background_photo = ImageTk.PhotoImage(background_image)
+background_label = tk.Label(root, image=background_photo)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 login_window = tk.Toplevel(root)
 login_window.title("Login")
-login_window.geometry("300x150")
+login_window.geometry("1280x720")
+
+background_image_login = Image.open("makanan.jpg")
+background_photo_login = ImageTk.PhotoImage(background_image_login)
+background_label_login = tk.Label(login_window, image=background_photo_login)
+background_label_login.place(x=0, y=0, relwidth=1, relheight=1)
 
 frame_login = ttk.Frame(login_window)
 frame_login.pack(pady=20)
 
-label_username = ttk.Label(frame_login, text="Username:")
-label_username.grid(row=0, column=0, sticky=tk.W)
-entry_username = ttk.Entry(frame_login)
-entry_username.grid(row=0, column=1)
+label_title = ttk.Label(login_window, text="Program Masak On Budget",font=("Segoe UI Black", 28))
+label_title.place(x=410,y=150)
 
-label_password = ttk.Label(frame_login, text="Password:")
-label_password.grid(row=1, column=0, sticky=tk.W)
-entry_password = ttk.Entry(frame_login, show="*")
-entry_password.grid(row=1, column=1)
+label_username = ttk.Label(login_window, text="Username:", font=("Times New Roman", 14))
+label_username.place(x=485,y=450)
+entry_username = ttk.Entry(login_window, font=("Times New Roman", 14))
+entry_username.place(x=570,y=450)
 
-button_login = ttk.Button(login_window, text="Login", command=login)
-button_login.pack()
+label_password = ttk.Label(login_window, text="Password:", font=("Times New Roman", 14))
+label_password.place(x=485, y=480)
+entry_password = ttk.Entry(login_window,font=("Times New Roman", 14), show="*")
+entry_password.place(x=570,y=480)
 
-button_register = ttk.Button(login_window, text="Register", command=register)
-button_register.pack()
+button_login = ttk.Button(login_window, text="Login", style="Biru.TButton", command=login)
+button_login.place(x=600,y=550)
 
+button_register = ttk.Button(login_window, text="Register", style="Merah.TButton", command=register)
+button_register.place(x=600,y=600)
 root.withdraw()
 
 frame_search = ttk.Frame(root)
-frame_search.pack(pady=20)
+frame_search.pack(pady=40)
 
-label_budget = ttk.Label(frame_search, text="Masukkan budget Anda:")
+label_budget = ttk.Label(frame_search, text="Masukkan budget Anda:", style="Custom.TLabel")
 label_budget.grid(row=0, column=0, sticky=tk.W)
 entry_budget = ttk.Entry(frame_search)
 entry_budget.grid(row=0, column=1)
 
-label_kategori = ttk.Label(frame_search, text="Masukkan kategori (appetizer/main course/dessert/diet food):")
+label_kategori = ttk.Label(frame_search, text="Masukkan kategori (appetizer/main course/dessert/diet food):", style="Custom.TLabel")
 label_kategori.grid(row=1, column=0, sticky=tk.W)
 entry_kategori = ttk.Entry(frame_search)
 entry_kategori.grid(row=1, column=1)
 
-button_search = ttk.Button(root, text="Cari", command=get_masakan_by_harga)
-button_search.pack()
+button_cari = ttk.Button(root, text="Cari", style="Biru.TButton", command=get_masakan_by_harga)
+button_cari.pack()
 
 frame_resep = ttk.Frame(root)
-frame_resep.pack(pady=20)
+frame_resep.pack(pady=40)
 
-label_pilihan = ttk.Label(frame_resep, text="Pilih nomor masakan untuk melihat resep:")
+label_pilihan = ttk.Label(frame_resep, text="Pilih nomor masakan untuk melihat resep:", style="Custom.TLabel")
 label_pilihan.grid(row=0, column=0, sticky=tk.W)
 entry_pilihan = ttk.Entry(frame_resep)
 entry_pilihan.grid(row=0, column=1)
 
-button_print_resep = ttk.Button(frame_resep, text="Print Resep", command=print_resep)
+button_print_resep = ttk.Button(frame_resep, text="Print Resep", style="Biru.TButton", command=lambda: print_resep(hasil_pencarian))
 button_print_resep.grid(row=1, column=0, pady=10)
 
-button_search_again = ttk.Button(frame_resep, text="Cari Lagi", command=search_again)
+button_search_again = ttk.Button(frame_resep, text="Cari Lagi", style="Green.TButton", command=search_again)
 button_search_again.grid(row=1, column=1, pady=10)
 
-result_text = tk.Text(root, height=15, width=50)
+button_logout = ttk.Button(root, text="Log Out", style="Merah.TButton", command=log_out)
+button_logout.pack()
+
+result_text = tk.Text(root, height=20, width=60)
 result_text.pack()
 result_text.config(state='disabled')
 
